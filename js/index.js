3f16a35eac4850a552c7c6df4e4168350f515ea6
whitestorm2346 <andrew770426@gmail.com>
2025-06-04 08:30:35 +0800

    the last update of the website using this account

--- js/index.js
+++ js/index.js
@@ -96,25 +96,8 @@ function changeOptionEng(option) {
     document.location.href = "index_eng.html?option=" + option;
 }
 
-$(document).ready(function () {
-    // 捲軸偵測距離頂部超過 50 才顯示按鈕
-    $(window).scroll(function () {
-      if ($(window).scrollTop() > 50) {
-        if ($(".back-top").hasClass("hide")) {
-          $(".back-top").toggleClass("hide");
-        }
-      } else {
-        $(".back-top").addClass("hide");
-      }
-    });
-  
-    // 點擊按鈕回頂部
-    $(".back-top").on("click", function (event) {
-      $("html, body").animate(
-        {
-          scrollTop: 0
-        },
-        500 // 回頂部時間為 500 毫秒
-      );
-    });
-  });
+document.addEventListener("DOMContentLoaded",() =>{
+  if(window.scrollY > 50){
+    scroll = 0;
+  } 
+})
